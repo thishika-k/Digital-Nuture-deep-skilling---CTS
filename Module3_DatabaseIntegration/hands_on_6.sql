97f874f4fe89ce41facbc2ed9568ac95f0b1a6af
Thishika Karthikeyan <sit23cs080@sairamtap.edu.in>
2026-07-02 15:23:54 +0530

    Added Hands-On 6

---
+++ Module3_DatabaseIntegration/hands_on_6.sql
@@ -0,0 +1,12 @@
+-- ORM INTEGRATION
+-- installing requirements
+-- pip install sqlalchemy mysql-connector-python
+-- ORM - object relational mapping - so instead of mentionin or wrting a lasrge set of data (tables) we can use python
+-- flow
+'''Python Object
+      ↓
+SQLAlchemy (Translator)
+      ↓
+SQL Query
+      ↓
+MySQL Database'''
