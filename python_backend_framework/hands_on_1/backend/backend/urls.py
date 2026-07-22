7408ca5f85bfaa7bb36835f12cfa3aa6c4f3cba4
Thishika Karthikeyan <sit23cs080@sairamtap.edu.in>
2026-07-22 05:46:31 +0530

    Completed Python Backend Hands-On 3

--- python_backend_framework/hands_on_1/backend/backend/urls.py
+++ python_backend_framework/hands_on_1/backend/backend/urls.py
@@ -1,22 +1,17 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from courses.views import (
+    DepartmentListCreateAPIView,
+    CourseListCreateAPIView,
+    StudentListCreateAPIView,
+    EnrollmentListCreateAPIView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('api/departments/', DepartmentListCreateAPIView.as_view()),
+    path('api/courses/', CourseListCreateAPIView.as_view()),
+    path('api/students/', StudentListCreateAPIView.as_view()),
+    path('api/enrollments/', EnrollmentListCreateAPIView.as_view()),
 ]
--- python_backend_framework/hands_on_1/backend/courses/views.py
+++ python_backend_framework/hands_on_1/backend/courses/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Department, Course, Student, Enrollment
+from .serializers import (
+    DepartmentSerializer,
+    CourseSerializer,
+    StudentSerializer,
+    EnrollmentSerializer,
+)
 
-# Create your views here.
+
+class DepartmentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+
+class CourseListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class StudentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+class EnrollmentListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Enrollment.objects.all()
+    serializer_class = EnrollmentSerializer
